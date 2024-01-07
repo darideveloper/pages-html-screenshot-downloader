773ad059b689697a53ff0d806e56070d88620250
darideveloper <darideveloper@gmail.com>
2024-01-06 18:54:10 -0600

    feat (initial commit)

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from logs import logger
+from scraping.web_scraping import WebScraping
+
+load_dotenv ()
+VAR = os.getenv ("VAR")
+
+def main (): 
+
+
+if __name__ == "__main__":
+    main()
